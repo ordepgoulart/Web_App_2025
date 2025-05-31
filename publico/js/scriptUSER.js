3daeb08db9e82b7af160ec90467c03b11bb3a852
ordepgoulart <pedrosa2843@gmail.com>
2025-05-31 11:21:08 -0300

    Esqueci de atualizar o index

--- publico/js/scriptUSER.js
+++ publico/js/scriptUSER.js
@@ -4,64 +4,63 @@ let listaDeUsuarios = [];
 let listaAux = [];
 let sizesID;
 
-function obterDadosClientes(){
+function obterDados(){
     fetch(urlBase, {
         method:"GET",
     }).then((resposta) => {
         if(resposta.ok)
             return resposta.json();
     }).then((lista)=>{
-        listaDeClientes = lista;
-        mostrarTabelaClientes();
+        listaDeUsuarios = lista;
+        if(listaDeUsuarios.length == 0)
+                sizesID = 0;
+        else sizesID = listaDeUsuarios.length;
     })
     .catch((erro) => {
         alert("ERRO AO TENTAR RECUPERAR AS INFORMAÇÕES DO SERVIDOR");
     });
 }
 
-function inserir(cliente){
+function inserir(user){
     fetch(urlBase, {
         "method":"POST",
         "headers": {
             "Content-Type": "aplication/json"
         },
-        "body" : JSON.stringify(cliente)
+        "body" : JSON.stringify(user)
     }).then((resposta) => {
         if(resposta.ok){
-            obterDadosClientes();
-            sizesID = listaDeClientes.length();
-        }
-            
+            obterDados();
+        }    
     })
     .catch((erro) => {
         alert("ERRO AO TENTAR INSERIR AS INFORMAÇÕES DO SERVIDOR");
     });
 }
 
-formulario.onsubmit=manipularSubmissao;
 
 function validar(user){
-    listaAux = listaDeClientes.filter((obj) => obj.cpf == user.cpf || obj.email == user.email);
+    listaAux = listaDeUsuarios.filter((obj) => obj.cpf == user.cpf || obj.email == user.email);
     if(listaAux.length > 0)
         return false;
     return true;
 }
 
-function manipularSubmissao(evento){
+function manipularSubmissaoUSER(evento){
     if (formulario.checkValidity()){
         const cpf = document.getElementById("cpf").value;
         const nome = document.getElementById("nome").value;
-        const telefone = document.getElementById("telefone").value;
-        const cidade = document.getElementById("cidade").value;
-        const uf = document.getElementById("uf").value;
-        const cep = document.getElementById("cep").value;
+        const email = document.getElementById("email").value;
+        const senha = document.getElementById("private").value;
+        const data = document.getElementById("data").value;
         const id = sizesID++;
-        const cliente = {cpf,nome,telefone,cidade,uf,cep,id};
-        if(validar(cliente)){
-            inserir(cliente);
+        const user = {cpf,nome,email,senha,data,id};
+        if(validar(user)){
+            inserir(user);
             formulario.reset();
+            alert("Usuário registrado com sucesso")
         }
-        else alert("!!! DADOS REDUNDANTES FORAM ENCONTRADOS !!!")
+        else alert("!!! O EMAIL OU CPF JÁ ESTÃO SENDO UTILIZADOS !!!");
     }
     else{
         formulario.classList.add('was-validated');
@@ -70,66 +69,6 @@ function manipularSubmissao(evento){
     evento.stopPropagation(); //impedindo que outros observem esse evento
 
 }
+formulario.onsubmit=manipularSubmissaoUSER;
 
-function mostrarTabelaClientes(){
-    const divTabela = document.getElementById("tabela");
-    divTabela.innerHTML=""; //apagando o conteúdo da div
-    if (listaDeClientes.length === 0){
-        divTabela.innerHTML="<p class='alert alert-info text-center'>Não há clientes cadastrados</p>";
-    }
-    else{
-        const tabela = document.createElement('table');
-        tabela.className="table table-striped table-hover";
-
-        const cabecalho = document.createElement('thead');
-        const corpo = document.createElement('tbody');
-        cabecalho.innerHTML=`
-            <tr>
-                <th>CPF</th>
-                <th>Nome</th>
-                <th>Telefone</th>
-                <th>Cidade</th>
-                <th>UF</th>
-                <th>CEP</th>
-                <th>Ações</th>
-            </tr>
-        `;
-        tabela.appendChild(cabecalho);
-        for (let i=0; i < listaDeClientes.length; i++){
-            const linha = document.createElement('tr');
-            linha.id=listaDeClientes[i].cpf;
-            linha.innerHTML=`
-                <td>${listaDeClientes[i].cpf}</td>
-                <td>${listaDeClientes[i].nome}</td>
-                <td>${listaDeClientes[i].telefone}</td>
-                <td>${listaDeClientes[i].cidade}</td>
-                <td>${listaDeClientes[i].uf}</td>
-                <td>${listaDeClientes[i].cep}</td>
-                <td><button type="button" class="btn btn-danger" onclick="excluirCliente('${listaDeClientes[i].id}','${listaDeClientes[i].cpf}')"><i class="bi bi-trash"></i></button></td>
-            `;
-            corpo.appendChild(linha);
-        }
-        tabela.appendChild(corpo);
-        divTabela.appendChild(tabela);
-
-    }
-}
-
-function excluirCliente(id,cpf){
-    if(confirm("Deseja realmente excluir o cliente " + cpf + "?")){
-        fetch(`${urlBase}/${id}`, {
-            "method":"DELETE",
-        })
-        .then((resposta) =>{
-            if(resposta.ok){
-                obterDadosClientes();
-                document.getElementById(cpf).remove(); //excluir a linha da tabela
-            }
-        })
-        .catch((erro) => {
-            alert("ERRO AO TENTAR EXCLUIR AS INFORMAÇÕES DO SERVIDOR");
-        });
-    }
-}
-
-obterDadosClientes();
+obterDados();
